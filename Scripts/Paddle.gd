54a6b49134bd8b63a9f7599cee1e3ea041dcefb6
F1r3f0x <plabin@outlook.cl>
2018-04-03 16:26:30 -0300

    Fix at getting the paddle controller

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -6,10 +6,13 @@ var player_inputs = {}
 
 
 func _ready():
-	match PLAYER_NUMBER:
+	match PADDLE_CONTROLLER:
 		0:
 			player_inputs = {"up":		"player_1_up",
 						     "down":	"player_1_down"}
 		1:
 			player_inputs = {"up":		"player_2_up",
 						     "down":	"player_2_down"}
+
+func _on_Paddle_area_entered(area):
+	print("PADDLE HIT!")
