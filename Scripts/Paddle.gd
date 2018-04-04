23b048f34ad257d1486415a87039f321933037b2
F1r3f0x <plabin@outlook.cl>
2018-04-04 00:40:14 -0300

    Paddle movement function

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,12 +1,27 @@
 extends Area2D
 
+# References
+onready var viewport_size = get_viewport_rect().size
+onready var sprite = $Sprite
+
+
+# Input Stuff
 enum CONTROLLER {PLAYER_1, PLAYER_2, GAME}
 export (int, "Player 1", "Player 2", "Game") var PADDLE_CONTROLLER
-
 var player_inputs = {}
 
 
+# Vars
+export (int) var SPEED
+var INITIAL_SPEED = SPEED
+var max_y = 0
+var min_y = 0
+
+
 func _ready():
+	min_y = sprite.texture.get_size().x / 2
+	max_y = viewport_size.y - sprite.texture.get_size().x / 2
+	
 	match PADDLE_CONTROLLER:
 		0:
 			player_inputs = {"up":		"player_1_up",
@@ -16,10 +31,22 @@ func _ready():
 						     "down":	"player_2_down"}
 
 func _process(delta):
+	# Input
 	if PADDLE_CONTROLLER != CONTROLLER.GAME:
 		if Input.is_action_pressed(player_inputs["up"]):
-			print("up")
-			# move up()
+			move(-1, delta)
 		if Input.is_action_pressed(player_inputs["down"]):
-			print("down")
-			# move down()
+			move(1, delta)
+
+	# Clamp Paddle
+	position.y = clamp(position.y, min_y, max_y)			
+
+
+func move(dir, delta):
+	position.y += dir * SPEED * delta
+
+
+
+func _on_Paddle_body_entered(body):
+	var ball = body
+	ball.direction.x *= -1
