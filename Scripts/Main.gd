1c68d3e7b5355441b0fc8063f503225f55a9c697
F1r3f0x <plabin@outlook.cl>
2018-04-09 21:10:14 -0300

    Messages

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -2,6 +2,8 @@
 # F1r3f0x - 2018
 extends Node2D
 
+export (PackedScene) var MessagesScene
+
 export (bool) var TESTING
 
 onready var viewport_rect = get_viewport_rect()
@@ -15,10 +17,10 @@ onready var paddle_right = $PaddleRight
 onready var label_score_left = $GUI/GUIElements/Score/ScoreLeft
 onready var label_score_right = $GUI/GUIElements/Score/ScoreRight
 
-enum GAMEMODES {GAMEMODE_VS, GAMEMODE_SOLO}
 var GAMESIDE = {"LEFT": -1, "RIGHT": 1}
 
-var gamemode
+onready var GAMEMODES = GameData.GAMEMODES
+onready var gamemode = GameData.gamemode
 
 var score_line_left_x = 0
 var score_line_right_x = 1024
@@ -30,24 +32,36 @@ var level = 0
 
 
 func _ready():
-	gamemode = GAMEMODES.GAMEMODE_VS
+	print(gamemode)
 	randomize()
 	
 	if TESTING:
 		ball.PLAYING = false
 	else:
 		new_game()
-	
 
 func new_game():
 	score_left = 0
 	score_right = 0
 	level = 0
 	
+	if gamemode == GAMEMODES.GAMEMODE_VS:
+		paddle_right.PADDLE_CONTROLLER = paddle_right.CONTROLLER.PLAYER_2
+	else:
+		paddle_right.PADDLE_CONTROLLER = paddle_right.CONTROLLER.GAME
+	
 	ball_start_dir = GAMESIDE.values()[randi() % GAMESIDE.size()]
 	ball.stop()
+	paddle_left.stop()
+	paddle_right.stop()
 	
 	setup_board()
+	
+	$GUI/Message.text = "Get Ready!"
+	$GUI/Message.show_off()
+	yield($GUI/Message, "finished")
+	$GUI/Message.fade()
+	yield($GUI/Message, "finished")
 	
 	$Timer.start()
 	yield($Timer, "timeout")
@@ -56,7 +70,10 @@ func new_game():
 		ball.RANDOM_START_DIRECTION = false
 	else:
 		ball.RANDOM_START_DIRECTION = true
-	ball.start()
+		
+	paddle_left.play()
+	paddle_right.play()
+	ball.play()
 	
 	
 func next_round():
---
+++ Scripts/Message.gd
@@ -0,0 +1,16 @@
+extends Label
+
+signal finished
+
+onready var timer = $CleanupTimer
+onready var animations = $Animations
+
+func show_off():
+	animations.play("show_off")
+	yield(animations,"animation_finished")
+	emit_signal("finished")
+	
+func fade():
+	animations.play("fade_out")
+	yield(animations,"animation_finished")
+	emit_signal("finished")
