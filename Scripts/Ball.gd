8d347bc7ddae46c18a8a16f92f32ca8d8c48b302
F1r3f0x <plabin@outlook.cl>
2018-04-11 22:35:34 -0300

    Added audio boop

--- Scripts/Ball.gd
+++ Scripts/Ball.gd
@@ -2,8 +2,6 @@
 # F1r3f0x - 2018
 extends KinematicBody2D
 
-onready var particles = $Particles
-
 const random_dir_y_min = -0.4
 const random_dir_y_max = 0.4
 
@@ -35,10 +33,13 @@ func play():
 func stop():
 	PLAYING = false
 	set_physics_process(false)
+	
+
+func boop():
+	$Audio.play()
 
 
 func _physics_process(delta):
-	
 	# Move ball every frame
 	move_and_collide(direction.normalized() * SPEED * delta)
 	
@@ -47,19 +48,21 @@ func _physics_process(delta):
 	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
 	
 	# Walls collision
-	if (direction.x >= 0 and position.x >= 1024) or (direction.x < 0 and position.x <=0):
-		direction.x *= -1
+	#if (direction.x >= 0 and position.x >= 1024) or (direction.x < 0 and position.x <=0):
+	#	direction.x *= -1
+	#	boop()
 		
 	if (direction.y >= 0 and position.y >= 600) or (direction.y < 0 and position.y <=0):
 		direction.y *= -1
+		boop()
 		
 	# Change particles direction
-	particles.process_material.gravity = Vector3(direction.x, direction.y, 0).normalized() * -50
+	$Particles.process_material.gravity = Vector3(direction.x, direction.y, 0).normalized() * -50
 	
 	if SPEED >= 100:
-		particles.emitting = true
+		$Particles.emitting = true
 	else:
-		particles.emitting = false
+		$Particles.emitting = false
 
 
 func get_random_direction():
